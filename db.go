ad803cbeeced192f5d60e24153d8e2d44a25d4b3
Tianyi <tianyi@insticator.com>
2020-01-02 15:35:34 -0500

    update maxmind db url

--- db.go
+++ db.go
@@ -35,7 +35,7 @@ var (
 	defaultDB = filepath.Join(os.TempDir(), "freegeoip", "db.gz")
 
 	// MaxMindDB is the URL of the free MaxMind GeoLite2 database.
-	MaxMindDB = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
+	MaxMindDB = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=LedU1GxlHCYmaRJG&suffix=tar.gz"
 )
 
 // DB is the IP geolocation database.
